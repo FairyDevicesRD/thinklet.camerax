b1d08181a12ba29f3efae1118dc279290329da82
Tamaki Hidetsugu <git@ralph.ms>
2024-11-08 01:42:45 +0900

    Add patch metadata script

---
+++ bin/patch-metadata.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+if [ $# -eq 1 ]; then
+  cd "$1"
+else
+  echo "./patch_metadata.sh [artifact directory]"
+  exit 1
+fi
+
+if [ "$(uname)" == 'Darwin' ]; then
+  if !(type "gsed" > /dev/null 2>&1); then
+    echo "Please install gnu-sed."
+    exit 1
+  fi
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  :
+else
+  echo "Not supported OS."
+  exit 1
+fi
+
+if !(type "mvn" > /dev/null 2>&1); then
+  echo "Please install mvn."
+  exit 1
+fi
+
+find . -type f -name "*.pom" -print0 | xargs gsed -i -e "s/^  <groupId>androidx.camera/  <groupId>ai.fd.thinklet/" -e "s/<artifactId>camera-video/<artifactId>camerax-camera-video/"
+find . -type f -name "*.module" -print0 | xargs gsed -i -e "s/^    \"group\": \"androidx.camera/    \"group\": \"ai.fd.thinklet/" -e "s/\"camera-video/\"camerax-camera-video/"
+
+echo "Patched to maven metadata."
